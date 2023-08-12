5e5126871e1fd1eeb464519bf51cf314d4a403a1
mirellesine <mirellesine@gmail.com>
2023-08-12 09:25:06 -0300

    Initial Commit

---
+++ WGJ23/WGJ23App.swift
@@ -0,0 +1,17 @@
+//
+//  WGJ23App.swift
+//  WGJ23
+//
+//  Created by Mirelle Sine on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WGJ23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
